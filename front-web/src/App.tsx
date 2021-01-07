f0cf1de5827c4df9f0a4190fa18360fcf955c567
ThiagoDC <cmsthiagoazevedo@gmail.com>
2021-01-07 18:04:04 -0300

    Add Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from "./Home";
 import Navbar from './NavBar';
 
 function App() {
   return (
-    <div >
+    <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
